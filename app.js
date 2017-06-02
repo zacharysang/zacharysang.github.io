045e1f35e3ee966a8353dfbd3e96bd1dedb260e5
Zachary Sang <zacharysang@gmail.com>
2017-06-02 18:27:00 -0400

    Removed unused routes folder

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -45,9 +41,6 @@ app.get(/^\/([a-z0-9-_]*)\/?$/i,function(req,res){
 
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
