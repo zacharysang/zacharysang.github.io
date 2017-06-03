b470a797f19e64c7a8fafaa192529c0e0404e450
Zachary Sang <zacharysang@gmail.com>
2017-06-03 09:48:03 -0400

    Added tagline carousel

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,6 +2,7 @@ var navColors = ['#F36F03','darkblue','green', 'teal','crimson','purple'];
 var mainColors = ['#F36F03','lightblue','lightgreen','#bef4f1','#ffe0e9','plum'];
 
 $(document).ready(function(){
+    //listen for mouseover on sectionContainer to trigger color change
     $('.sectionContainer').on('mouseover',function(ev){
 
         //get id of current element
@@ -13,17 +14,32 @@ $(document).ready(function(){
 
         $('.gridContainer').css({"transition":"0.3s","background-color":mainCol});
         $('.navbar').css({"transition":"0.3s","background-color":navCol});
-        $('*::selection').css({'transition': '0.3s','background-color': mainCol});
 
     });
 
+    //listen for mouseover banner to reset color
     $('.banner').on('mouseover', function(ev){
         $('.gridContainer').css({"transition":"0.3s","background-color":'white'});
         $('.navbar').css({"transition":"0.3s","background-color":'#F36F03'});
     })
+
+    slogPos = 0;
+    slogans = $('.tagline');
+    window.setInterval(rotateSlogan, 5000);
+
 });
 
 
 function resetBaseColor(col){
+
+}
+
+function rotateSlogan(){
+    $(slogans[slogPos]).hide();
+    $(slogans[(slogPos+1) % slogans.length]).show();
+    
+    slogPos = (++slogPos) % slogans.length;
+
+
 
 }
