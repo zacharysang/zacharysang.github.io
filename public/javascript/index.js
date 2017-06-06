e37f249aa9d16c9d8edcb52a841965724ef1e7fe
Zachary Sang <zacharysang@gmail.com>
2017-06-05 22:30:55 -0400

    Added more content. fixed some styling.

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,5 +1,5 @@
 var navColors = ['rgba(243,111,3,0.7)','rgba(0,0,139,0.7)','rgba(0,128,0,0.7)', 'rgba(0,128,128,0.7)','rgba(220,20,60,0.7)','rgba(128,0,128,0.7)'];
-var mainColors = ['#F36F03','lightblue','lightgreen','#bef4f1','#ffe0e9','plum'];
+var mainColors = ['#FFA962','#DAF0F8','#DEFEDE','#ecfefd','#ffe7ff','#F2CEF2'];
 
 $(document).ready(function(){
     //listen for mouseover on sectionContainer to trigger color change
@@ -26,20 +26,23 @@ $(document).ready(function(){
 
     //listen for scroll and check position of top of screen, replace the mouseover stuff
 
+
+
     //listen for click on the h1 elements
     $('.sectionHeadings h2').on('click',function(ev){
-        
-        
-        $('.sectionContent .entry').each(function(){$(this).animate({opacity: 0},100)});
+        $('.sectionContent .entry').each(function(){
+            $(this).animate({opacity: 0},100);
+            $(this).hide();
+        });
 
-        $(`.sectionContent .${$(ev.target).attr('class')}`).animate({opacity: 1},100);
+        $(`.sectionContent .${$(ev.target).attr('class')}`).animate({opacity: 1},100).show();
 
         //highlight the heading
         $('.sectionHeadings h2').each(function(){$(this).css({'background-color':'inherit','color':'inherit','transition':'0.2s'})});
         $(ev.target).css({'background-color': navCol, 'color': 'white', 'transition': '0.2s'});
 
         //^maybe just apply a class?
-        
+       
 
     });
 
@@ -52,13 +55,13 @@ $(document).ready(function(){
 });
 
 
-function resetBaseColor(col){
+function setMainCol(){
 
 }
 
 function rotateSlogan(){
-    $(slogans[slogPos]).hide();
-    $(slogans[(slogPos+1) % slogans.length]).show();
+    $(slogans[slogPos]).addClass('hide');
+    $(slogans[(slogPos+1) % slogans.length]).removeClass('hide');
     
     slogPos = (++slogPos) % slogans.length;
 
