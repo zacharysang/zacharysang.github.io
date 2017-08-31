704db15bd32b7893041c732227e6c7598faf045a
EC2 Default User <ec2-user@ip-172-31-12-13.us-west-2.compute.internal>
2017-08-31 18:44:00 +0000

    forced https redirection

--- keystone.js
+++ keystone.js
@@ -30,7 +30,7 @@ keystone.init({
 
 	'port': process.env.PORT || 3000,
 	'ssl port': process.env.SSL_PORT || 3001,
-	'ssl': 'true',
+	'ssl': 'force',
 	'ssl cert': letsencryptPath + 'cert.pem',
 	'ssl key': letsencryptPath + 'privkey.pem',
 	letsencrypt: {
